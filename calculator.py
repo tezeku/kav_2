e2129abe229f8a9a4e81bc40dd9ec2851329f67c
tezeku <cuzyuhin@yandex.ru>
2025-12-10 15:48:55 +0300

    Replace func

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
 import math
 
-def get_area(a, b):
-    return a * b
-
 def get_hypotenuse(a, b):
     return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
+
+def get_area(a, b):
+    return a * b
 
 if __name__ == "__main__":
     print("Введите a:")
